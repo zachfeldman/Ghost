e0fbfcc88c3cd81a4182cc70cb0a6b05164b5c56
Zach Feldman <zachfeldman@gmail.com>
2013-10-14 23:27:53 -0400

    Config.

---
+++ config.js
@@ -0,0 +1,107 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+
+var port = process.env.PORT || CONFIG.port;
+var path = require('path'),
+    config;
+
+config = {
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        url: 'http://test-ghost.herokuapp.com/',
+
+        mail: {
+         transport: 'SMTP',
+         options: {
+             service: 'Mailgun',
+             auth: {
+                 user: 'postmaster@wearyrabbit.mailgun.org',
+                 pass: '3hdn6evfzfb3'  // mailgun password
+             }
+         }
+        },
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: port
+        }
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://test-ghost.herokuapp.com/',
+        mail: {
+         transport: 'SMTP',
+         options: {
+             service: 'Mailgun',
+             auth: {
+                 user: 'postmaster@wearyrabbit.mailgun.org',
+                 pass: '3hdn6evfzfb3'  // mailgun password
+             }
+         }
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: port
+        }
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: port
+        }
+    },
+
+    // ### Travis
+    // Automated testing run through Github
+    travis: {
+        url: 'http://127.0.0.1:2368',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-travis.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2368'
+        }
+    }
+};
+
+// Export config
+module.exports = config;
